9d9ea0fdaa9fead184e49c597084817982c21c26
GustavoSantoz <Guga_Vine@outlook.com>
2024-07-14 02:24:47 -0300

    Fix: Test get text element

--- src/components/ButtonTalk/ButtonTalk.test.tsx
+++ src/components/ButtonTalk/ButtonTalk.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { ButtonTalk } from './ButtonTalk';
 
 test('renders ButtonTalk', () => {
-  const { getByText } = render(<ButtonTalk children={undefined} mode={undefined}/>);
-  const element = getByText(/ButtonTalk/i);
+  const { getByText } = render(<ButtonTalk children="Test" mode={undefined}/>);
+  const element = getByText(/Test/i);
   expect(element).toBeInTheDocument();
 });
