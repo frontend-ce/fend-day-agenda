64d6229575d3aeaa3f1779e35def54033a698b9d
unknown <rodrigonetogithub@gmail.com>
2024-07-14 02:17:58 -0300

    fix: Correcting props

--- src/components/DeadComponent/DeadComponent.test.tsx
+++ src/components/DeadComponent/DeadComponent.test.tsx
@@ -3,6 +3,6 @@ import { DeadComponent } from './DeadComponent';
 
 test('renders DeadComponent', () => {
   const { getByText } = render(<DeadComponent title={'Abertura'} hours={'8:00y'} />);
-  const element = getByText(/DeadComponent/i);
+  const element = getByText(/Abertura/i);
   expect(element).toBeInTheDocument();
 });
