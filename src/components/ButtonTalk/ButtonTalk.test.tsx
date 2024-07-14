a2a88b33c1e83bda2e9f110fa177eb7176aad54d
GustavoSantoz <Guga_Vine@outlook.com>
2024-07-14 02:07:54 -0300

    fix: Props test

--- src/components/ButtonTalk/ButtonTalk.test.tsx
+++ src/components/ButtonTalk/ButtonTalk.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { ButtonTalk } from './ButtonTalk';
 
 test('renders ButtonTalk', () => {
-  const { getByText } = render(<ButtonTalk />);
+  const { getByText } = render(<ButtonTalk children={undefined} mode={undefined}/>);
   const element = getByText(/ButtonTalk/i);
   expect(element).toBeInTheDocument();
 });
