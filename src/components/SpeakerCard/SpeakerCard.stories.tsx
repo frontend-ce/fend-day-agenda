c57b7f92a9daa1bd1f8beb97a8fa939831db157d
João Vitor <jvmnc232@gmail.com>
2024-07-14 05:22:42 -0300

    feat: add speaker card with props

--- src/components/SpeakerCard/SpeakerCard.stories.tsx
+++ src/components/SpeakerCard/SpeakerCard.stories.tsx
@@ -1,23 +1,32 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { SpeakerCard } from "./SpeakerCard"
+import type { Meta, StoryObj } from "@storybook/react";
+import { SpeakerCard } from "./SpeakerCard";
 
-export type Story = StoryObj<typeof SpeakerCard>
+export type Story = StoryObj<typeof SpeakerCard>;
 
 const meta: Meta<typeof SpeakerCard> = {
-  title: 'Components/SpeakerCard',
+  title: "Components/SpeakerCard",
   component: SpeakerCard,
   tags: ["autodocs"],
   argTypes: {
     label: { control: "text" },
-    tags: { control: "object" }
+    tags: { control: "object" },
+    imageFallback: { control: "text" },
+    imageUrl: { control: "text" },
+    name: { control: "text" },
+    role: { control: "text" },
+    onChangeMode: { action: "mode changed" },
   },
-} satisfies Meta<typeof SpeakerCard>
+} satisfies Meta<typeof SpeakerCard>;
 
 export default meta;
 
 export const Default: Story = {
-  args:{
-    label: "How to get away with murder",
-    tags: ['Tag1', 'Tag1', 'Tag1' ]
-  }
-}
+  args: {
+    label: "Typescript morreu?!",
+    tags: ["Frontend", "Convida", "Comunidade"],
+    imageFallback: "A",
+    imageUrl: "",
+    name: "Abraão",
+    role: "Developer",
+  },
+};
--- src/components/SpeakerCard/types.ts
+++ src/components/SpeakerCard/types.ts
@@ -1,4 +1,7 @@
-export interface SpeakerCardProps {
+import { ProfileCardProps } from "../ProfileCard/types"
+
+export interface SpeakerCardProps extends ProfileCardProps {
     label: string
     tags: string[]
+    onChangeMode: (mode: boolean) => void
 }
