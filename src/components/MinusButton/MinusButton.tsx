4f3944697661cfe2c7c5adb1026f236b05ce3c07
Janaina <jmtannus@gmail.com>
2024-07-14 00:54:08 -0300

    changing the transparent button

--- src/components/MinusButton/MinusButton.tsx
+++ src/components/MinusButton/MinusButton.tsx
@@ -4,10 +4,9 @@ import { CircleMinus } from "lucide-react"
 export const MinusButton = () => {
   return (
     
-    <Button className="rounded-full" variant="outline" size="icon">
-    <CircleMinus className="h-8 w-8" />
+    <Button className="rounded-full bg-transparent border-transparent" variant="outline" size="icon">
+    <CircleMinus className="h-8 w-8 bg-transparent border-transparent" />
   </Button>
     
   );
 };
-
--- src/components/PlusButton/PlusButton.tsx
+++ src/components/PlusButton/PlusButton.tsx
@@ -4,8 +4,8 @@ import { CirclePlus } from "lucide-react"
 export const PlusButton = () => {
   return (
     
-      <Button className="rounded-full" variant="outline" size="icon">
-     <CirclePlus className="h-8 w-8" />
+      <Button className="rounded-full bg-transparent border-transparent" variant="outline" size="icon">
+     <CirclePlus className="h-8 w-8 bg-transparent border-transparent" />
      </Button>
     
   );
