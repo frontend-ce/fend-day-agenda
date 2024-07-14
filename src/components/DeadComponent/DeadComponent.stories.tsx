4d8bec81acc84bf70d2c912a52507ea3da2947b7
unknown <rodrigonetogithub@gmail.com>
2024-07-14 02:10:00 -0300

    feat: add props

--- src/components/DeadComponent/DeadComponent.stories.tsx
+++ src/components/DeadComponent/DeadComponent.stories.tsx
@@ -6,8 +6,25 @@ export type Story = StoryObj<typeof DeadComponent>
 const meta: Meta<typeof DeadComponent> = {
   title: 'Components/DeadComponent',
   component: DeadComponent,
+  argTypes: {
+    title: {
+      control: {
+        type: 'text',
+      }, 
+    },
+    hours: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } satisfies Meta<typeof DeadComponent>
 
 export default meta;
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    title: 'Dead Component',
+    hours: '12',
+  },
+}
--- src/components/DeadComponent/DeadComponent.test.tsx
+++ src/components/DeadComponent/DeadComponent.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { DeadComponent } from './DeadComponent';
 
 test('renders DeadComponent', () => {
-  const { getByText } = render(<DeadComponent />);
+  const { getByText } = render(<DeadComponent title={'Abertura'} hours={'8:00y'} />);
   const element = getByText(/DeadComponent/i);
   expect(element).toBeInTheDocument();
 });
