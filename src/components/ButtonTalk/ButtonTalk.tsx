ddbbd50a337c51c05a7aa2c2fc07a7cbfabb25ae
GustavoSantoz <Guga_Vine@outlook.com>
2024-07-14 00:43:37 -0300

    style: Update Width for Fixed 154px

--- src/components/ButtonTalk/ButtonTalk.tsx
+++ src/components/ButtonTalk/ButtonTalk.tsx
@@ -2,6 +2,6 @@ import { Button } from "../ui/button";
 
 export const ButtonTalk = () => {
   return (
-    <Button className="bg-[#832BD8]/[.20] border-[0.5px] border-[#A855F7] hover:bg-[#A855F7] focus:bg-[#A855F7] rounded-[5px]">Front-End</Button>
+    <Button className="bg-[#832BD8]/[.20] border-[0.5px] border-[#A855F7] hover:bg-[#A855F7] focus:bg-[#A855F7] rounded-[5px] w-[154px]">Front-End CE</Button>
   );
 };
