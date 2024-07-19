005a4457327eff5260a791b180a7621230bc7bde
João Vitor <jvmnc232@gmail.com>
2024-07-19 11:20:33 -0300

    feat: add component in route

--- src/pages/MyAgenda/MyAgenda.tsx
+++ src/pages/MyAgenda/MyAgenda.tsx
@@ -11,7 +11,15 @@ export const MyAgenda = () => {
   const { data } = useAgenda();
   const { savedCardIds, toggleSaveCard } = useSavedTalks();
 
-  const savedTalks = data?.Convida.filter((item: Palestra) =>
+  const savedTalksConvida = data?.Convida.filter((item: Palestra) =>
+    savedCardIds.includes(item.id)
+  );
+
+  const savedTalksFrontend = data?.Frontend.filter((item: Palestra) =>
+    savedCardIds.includes(item.id)
+  );
+
+  const savedTalksComunidades = data?.Comunidades.filter((item: Palestra) =>
     savedCardIds.includes(item.id)
   );
 
@@ -24,8 +32,36 @@ export const MyAgenda = () => {
         <LinkAgenda />
       </div>
       <DeadComponent title={"Abertura"} hours={"8:00"} />
-      <div className="mt-8 flex flex-col items-center justify-center gap-3">
-        {savedTalks?.map((item: Palestra) => (
+      <div className="flex flex-col items-center gap-6 mt-6 w-full">
+      {savedTalksFrontend?.map((item: Palestra) => (
+          <SpeakerCard
+            key={item.id}
+            hour={item.hour}
+            label={item.title}
+            tags={item.tags}
+            name={item.speaker.title}
+            role={item.speaker.role}
+            imageUrl={item.speaker.image}
+            imageFallback={item.speaker.title}
+            isSaved={true} 
+            onChangeMode={() => toggleSaveCard(item.id)}
+          />
+        ))}
+         {savedTalksComunidades?.map((item: Palestra) => (
+          <SpeakerCard
+            key={item.id}
+            hour={item.hour}
+            label={item.title}
+            tags={item.tags}
+            name={item.speaker.title}
+            role={item.speaker.role}
+            imageUrl={item.speaker.image}
+            imageFallback={item.speaker.title}
+            isSaved={true} 
+            onChangeMode={() => toggleSaveCard(item.id)}
+          />
+        ))}
+        {savedTalksConvida?.map((item: Palestra) => (
           <SpeakerCard
             key={item.id}
             hour={item.hour}
