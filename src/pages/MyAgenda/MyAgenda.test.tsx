93429e5a111f9b213d7fa6ba5515035db97b61ea
Gustavo dos Santos <Guga_Vine@outlook.com>
2024-07-19 13:15:11 -0300

    Test: fix queryClient

--- src/pages/MyAgenda/MyAgenda.test.tsx
+++ src/pages/MyAgenda/MyAgenda.test.tsx
@@ -1,8 +1,17 @@
 import { render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
 import { MyAgenda } from './MyAgenda';
 
-test('renders MyAgenda', () => {
-  const { getByText } = render(<MyAgenda />);
-  const element = getByText(/MyAgenda/i);
+test('renders LivePage', () => {
+  const queryClient = new QueryClient();
+  const { getByText } = render(
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <MyAgenda />
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+  const element = getByText(/Nenhuma palestra salva para exibir./i);
   expect(element).toBeInTheDocument();
 });
