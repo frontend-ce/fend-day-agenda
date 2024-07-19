2bd1165daa3c3960ead44e61816385a6338d0a09
João Vitor <jvmnc232@gmail.com>
2024-07-19 13:50:19 -0300

    feat: adjust github actions with env

--- src/pages/LivePage/LivePage.test.tsx
+++ src/pages/LivePage/LivePage.test.tsx
@@ -1,9 +1,9 @@
-import { render } from '@testing-library/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter } from 'react-router-dom';
-import { LivePage } from './LivePage';
+import { render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
+import { LivePage } from "./LivePage";
 
-test('renders LivePage', () => {
+test("renders LivePage", () => {
   const queryClient = new QueryClient();
   const { getByText } = render(
     <QueryClientProvider client={queryClient}>
