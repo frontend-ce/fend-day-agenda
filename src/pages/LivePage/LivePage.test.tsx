e87a09c9605d121457b1cc7490f8dfda4db8cc82
João Vitor <jvmnc232@gmail.com>
2024-07-19 06:05:56 -0300

    feat: add browser-router to livePage test

--- src/pages/LivePage/LivePage.test.tsx
+++ src/pages/LivePage/LivePage.test.tsx
@@ -1,12 +1,15 @@
 import { render } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
 import { LivePage } from './LivePage';
 
 test('renders LivePage', () => {
   const queryClient = new QueryClient();
   const { getByText } = render(
     <QueryClientProvider client={queryClient}>
-      <LivePage />
+      <BrowserRouter>
+        <LivePage />
+      </BrowserRouter>
     </QueryClientProvider>
   );
   const element = getByText(/Acontecendo agora:/i);
