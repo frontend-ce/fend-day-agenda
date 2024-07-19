2de36cdd7e313884554b46a2ad81c2a6c9b651b8
João Vitor <jvmnc232@gmail.com>
2024-07-19 13:24:32 -0300

    feat: update schema in enviroment.ts

--- env.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-const envSchema = z.object({
-    VITE_SENTRY_DSN: z.string(),
-    VITE_TRACE_PROPAGATION_TARGETS: z.array(z.string()),
-    VITE_API_URL : z.string().min(1),
-  });
-
-export const env = envSchema.parse(process.env);
