43b8b1186bf12dc9058352bbe1507ae0fef4c6a7
Gabriel GC <gabrielcccruz@hotmail.com>
2024-07-18 23:49:46 -0300

    fix: Typerror resolved

--- src/components/ReturnButton/ReturnButton.stories.tsx
+++ src/components/ReturnButton/ReturnButton.stories.tsx
@@ -1,13 +1,12 @@
+// src/components/ReturnButton/ReturnButton.stories.tsx
 import { Meta, StoryFn } from '@storybook/react';
 import { ReturnButton } from './ReturnButton';
 import { BrowserRouter } from 'react-router-dom';
-
 
 export default {
   title: 'Components/ReturnButton',
   component: ReturnButton,
 } as Meta;
-
 
 const Template: StoryFn = () => (
   <BrowserRouter>
--- src/components/ReturnButton/ReturnButton.test.tsx
+++ src/components/ReturnButton/ReturnButton.test.tsx
@@ -1,8 +1,13 @@
 import { render } from '@testing-library/react';
-import { ReturnButton } from './ReturnButton';
+import { BrowserRouter } from 'react-router-dom';
+import ReturnButton from './ReturnButton';
 
 test('renders ReturnButton', () => {
-  const { getByText } = render(<ReturnButton />);
-  const element = getByText(/ReturnButton/i);
-  expect(element).toBeInTheDocument();
+  const { getByRole } = render(
+    <BrowserRouter>
+      <ReturnButton />
+    </BrowserRouter>
+  );
+  const linkElement = getByRole('link');
+  expect(linkElement).toBeInTheDocument();
 });
