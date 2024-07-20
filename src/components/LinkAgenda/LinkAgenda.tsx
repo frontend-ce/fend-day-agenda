8d23c4d12604f1121000a0d3600b741622e8978e
RodrigoNet0 <rodrigonetogithub@gmail.com>
2024-07-19 22:47:42 -0300

    fix: erros de layout

--- src/components/LinkAgenda/LinkAgenda.tsx
+++ src/components/LinkAgenda/LinkAgenda.tsx
@@ -2,19 +2,17 @@ import { Link } from "react-router-dom";
 
 export const LinkAgenda = () => {
   return (
-    <div className="flex justify-center  items-center">
-      <div className="text-white flex flex-row justify-between items-center p-2 md:p-1 space-x-4 md:space-x-6">
-        <Link
-          className="underline text-[12px] "
-          to="/lives"
-        >
+    <div className="flex justify-center items-center">
+      <div className="flex flex-row justify-between items-center p-2 md:p-1 space-x-4 md:space-x-6">
+        <Link className="text-white underline text-[12px] md:text-sm" to="/lives">
           Acontecendo agora
         </Link>
         <Link
-          className="underline text-[12px] md:text-sm flex-1 text-center p-2 md:p-1"
+          className="text-white underline text-[12px] md:text-sm flex-1 text-center p-2 md:p-1"
           to="/agenda"
         >
-          Ver minha agenda &gt;
+          Ver minha 
+          agenda &gt;
         </Link>
       </div>
     </div>
