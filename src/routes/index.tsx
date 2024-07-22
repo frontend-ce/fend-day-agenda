292681c7d4de9f8bc7cfd64d87abbfe89b1c2d8f
João Vitor <jvmnc232@gmail.com>
2024-07-21 21:46:02 -0300

    refactor: remove unnecessary routes in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,8 +8,6 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/agenda" element={<h1>Minha Agenda</h1>} />
-      <Route path="/" element={<h1>Home</h1>} />
       <Route path="/agenda" element={<MyAgenda/>} />
       <Route path="/lives" element={<LivePage />} />
       <Route path="*" element={<h1>404</h1>} />
